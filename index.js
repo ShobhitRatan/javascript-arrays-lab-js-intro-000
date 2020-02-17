18a109615dca2bb90dac4dc382103d24330f50fb
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-17 02:42:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function destructivelyAppendKitten(name) {
   kittens = kittens.concat(name);
   return kittens;
 }
+
+function destructivelyPrependKitten(name) {
+  kittens = kittens.unshift(name);
+  return kittens;
+}
